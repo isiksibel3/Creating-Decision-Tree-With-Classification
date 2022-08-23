38987e743ad2493d16cf95866170e45bd0fc58c9
Sibel IŞIK GÜLER <isiksibel3@gmail.com>
2022-08-23 16:14:56 +0300

    Add files via upload
    Reduction Process Why Is It Necessary?
    Reduction process narrows our data. That is, determine specific ranges for each of the available data. Reducing the density and diversity provides a kind of grouping of data. That will occur unless the reduction process is performed for data of a decision tree for each different situation, assesses and expands sideways. A good decision-tree-like structure should not be excessive sideways expansion.
    
    The data values in the data set that we use nested and dispersed. For this reason, we wrote a library will make this process automatic and by the name of DataMining.Engine.Reduction. While doing this process the “3-4-5” rule applied.
    
    3-4-5 Rule Method
    The general functioning of this algorithm is as follows:
    Inputs: 
    One-dimensional data set.
    Steps:
    1.	Sort the data. From small to large or large to small.
    2.	Highest %5 and lowest 5% take off the upper and lower part from the data base.
    3.	Find the most obvious step (MOS) between the largest and smallest values of the remaining two data.
    4.	Calculate the number of ranges (NOR) between two MOS value.
    if ( NOR = 3 or 6 or 7 or 9)
    - Split the data into three distinct groups.
    - Separating criteria = (New maximum – New minimum) /3
                  -      if(NOR = 2 or 4 or 8)
                         - Split the data into four distinct groups..
                         - Separating criteria = (New maximum – New minimum) /4
                  -      if(NOR = 1 or 5 or 10)
                         - Split the data into five distinct groups.
                         - Separating criteria = ((New maximum – New minimum) /5
    
    5.	In the second step that are thrown %5 of the top and bottom as the two groups add.

---
+++ Reduction.cs
@@ -0,0 +1,182 @@
+﻿using HelpException;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMining.Engine.Reduction
+{
+    public class Reduction
+    {
+        public DataTable Reduce(DataTable filledMissingValuesCopyMainDataTable)
+        {
+            try
+            {
+                DataTable returnDataTable = filledMissingValuesCopyMainDataTable.Clone();
+
+
+                foreach (DataColumn mainDataTableColumn in filledMissingValuesCopyMainDataTable.Columns)
+                {
+                    returnDataTable.Columns[mainDataTableColumn.ColumnName].DataType = typeof(String);
+                }
+                foreach (DataRow row in filledMissingValuesCopyMainDataTable.Rows)
+                {
+                    returnDataTable.Rows.Add(row.ItemArray);
+                }
+                for (int i = 0; i < filledMissingValuesCopyMainDataTable.Columns.Count; i++)
+                {
+                    if (filledMissingValuesCopyMainDataTable.Columns[i].DataType == typeof(int))
+                    {
+                        List<int> values = (from value in filledMissingValuesCopyMainDataTable.AsEnumerable()
+                                            select Convert.ToInt32(value[filledMissingValuesCopyMainDataTable.Columns[i].ColumnName])).ToList();
+                        List<int> distinctedValues = values.Distinct().ToList();
+
+
+                        values.Sort();
+                        distinctedValues.Sort();
+
+                        List<int> minValuesFivePercent = new List<int>();
+                        List<int> maxValuesFivePercent = new List<int>();
+                        Dictionary<string, List<int>> percentageMinMaxValues = calculateMinMaxFivePercentValue(distinctedValues);
+                        minValuesFivePercent = percentageMinMaxValues["min"];
+                        maxValuesFivePercent = percentageMinMaxValues["max"];
+
+                        if (minValuesFivePercent.Count == 0 || maxValuesFivePercent.Count == 0)
+                        {
+                            minValuesFivePercent.Add(distinctedValues.Min());
+                            maxValuesFivePercent.Add(distinctedValues.Max());
+                        }
+
+                        List<int> prunedValues = distinctedValues;
+                        foreach (int value in minValuesFivePercent)
+                            prunedValues.Remove(value);
+                        foreach (int value in maxValuesFivePercent)
+                            prunedValues.Remove(value);
+
+                        int minValue = prunedValues.Min();
+                        int maxValue = prunedValues.Max();
+
+                        string minValueString = Math.Abs(minValue).ToString(); //Eski değer gelme ihtimaline karşı
+                        string maxValueString = Math.Abs(maxValue).ToString();
+
+                        int mostEffectiveStep = 0;
+                        int range = 0;
+
+                        if (maxValueString.Length > minValueString.Length)
+                        {
+                            mostEffectiveStep = Convert.ToInt32(maxValueString[0].ToString());
+                            if (mostEffectiveStep == 1)
+                            {
+                                range = Convert.ToInt32(maxValueString[0].ToString());
+                            }
+                            else
+                            {
+                                range = Convert.ToInt32(maxValueString[0].ToString());
+                            }
+                        }
+                        else
+                        {
+                            for (int j = 0; j < minValueString.Length; j++)
+                            {
+                                if (maxValueString[j] > minValueString[j])
+                                {
+                                    mostEffectiveStep = Convert.ToInt32(maxValueString[j].ToString());
+                                    range = maxValueString[j] - minValueString[j];
+                                    break;
+                                }
+                            }
+                        }
+                        int groupCount = 0;
+                        switch (range)
+                        {
+                            case 3:
+                            case 6:
+                            case 7:
+                            case 9:
+                                groupCount = 3;
+                                break;
+                            case 2:
+                            case 4:
+                            case 8:
+                                groupCount = 4;
+                                break;
+                            case 1:
+                            case 5:
+                            case 10:
+                                groupCount = 5;
+                                break;
+                            default:
+                                break;
+                        }
+
+                        List<string> groups = new List<string>();
+                        if (groupCount != 0)
+                        {
+                            int increment = (maxValue - minValue) / groupCount;
+                            groups.Add(minValuesFivePercent.Min().ToString() + "-" + minValue.ToString());
+                            int sum = minValue;
+                            for (int j = 0; j < groupCount; j++)
+                            {
+                                string group = sum.ToString() + "-" + (sum += increment).ToString();
+                                groups.Add(group);
+                            }
+                            groups.Add(maxValue.ToString() + "-" + maxValuesFivePercent.Max().ToString());
+                        }
+                        else // No range
+                        {
+                            groups.Add(minValuesFivePercent.Min().ToString() + "-" + maxValuesFivePercent.Max());
+                        }
+                        foreach (DataRow rw in returnDataTable.Rows)
+                        {
+                            foreach (string group in groups)
+                            {
+                                if (Convert.ToInt32(rw[i].ToString()) <= Convert.ToInt32(group.Split('-')[1]))
+                                {
+                                    rw[i] = group;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                }
+                return returnDataTable;
+            }
+            catch (Exception ex)
+            {
+                throw HException.Save(ex, "DataMining.Engine.Reduction.Reduction", "Reduce",
+                    new Parameter() { Name = "filledMissingValuesCopyMainDataTable", Value = "Type of DataTable" });
+            }
+        }
+        private Dictionary<string, List<int>> calculateMinMaxFivePercentValue(List<int> values)
+        {
+            try
+            {
+                Dictionary<string, List<int>> returnMinMaxValues = new Dictionary<string, List<int>>();
+                List<int> minValues = new List<int>();
+                List<int> maxValues = new List<int>();
+                int percentage = 5;
+                while (minValues.Count < 2)
+                {
+                    double d = (Convert.ToDouble(values.Count) * Convert.ToDouble(percentage)) / Convert.ToDouble(100);
+                    int round = Convert.ToInt32(Math.Round(d, 2));
+                    minValues = values.Take(round).ToList();
+                    maxValues = values.Skip(values.Count - round).Take(round).ToList();
+                    percentage += 5;
+                }
+                returnMinMaxValues.Add("min", minValues);
+                returnMinMaxValues.Add("max", maxValues);
+
+                return returnMinMaxValues;
+            }
+            catch (Exception ex)
+            {
+                throw HException.Save(ex, "DataMining.Engine.Reduction.Reduction", "calculateMinMaxFivePercentValue",
+                   new Parameter() { Name = "values", Value = values });
+            }
+        }
+
+    }
+}
